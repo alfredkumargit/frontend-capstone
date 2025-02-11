a0c9056a5c647a6c925de344ac59594aee035f0f
Alfred <alfredkumar@gmail.com>
2025-02-11 20:53:28 +1100

    Update components directory

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Nav from './Nav';
 
 const Header = () => {
   return (
-    <>
-      <div>Header</div>
-    </>
+
+    <header className="header">
+    <a href="/"><img src="https://little-lemon-react-capstone.vercel.app/static/media/little-lemon.d4cae6fac8384c2dae04.png" width="200" height="55" alt="Logo"></img></a>
+    <Nav />
+  </header>
+
   );
 }
 
