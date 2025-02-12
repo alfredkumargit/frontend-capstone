b49312d2af11c7c5cfa34c468ebc173155de2571
Alfred <alfredkumar@gmail.com>
2025-02-12 17:11:05 +1100

    Add new feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import Nav from './Nav';
 
 const Header = () => {
   return (
-
-    <header className="header">
-    <a href="/"><img src="https://little-lemon-react-capstone.vercel.app/static/media/little-lemon.d4cae6fac8384c2dae04.png" width="200" height="55" alt="Logo"></img></a>
-    <Nav />
-  </header>
-
+    <header className="navigation">
+      <a href="/"><img src="https://little-lemon-react-capstone.vercel.app/static/media/little-lemon.d4cae6fac8384c2dae04.png" width="200" height="55" alt="Logo" /></a>
+      <Nav />
+    </header>
   );
 }
 
